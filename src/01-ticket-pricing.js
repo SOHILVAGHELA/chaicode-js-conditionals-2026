e949a8f31a50fe6e39d1555b3c49072f11dc1217
SOHILVAGHELA <sohilvaghela7863@gmail.com>
2026-06-21 23:32:48 +0530

    Complete challenge 1

--- src/01-ticket-pricing.js
+++ src/01-ticket-pricing.js
@@ -24,4 +24,24 @@
  */
 export function getTicketPrice(age, isWeekend) {
   // Your code here
+  if(typeof age !=="number" || age<0){
+    return -1
+  }
+   let price;
+  if(age >=0 && age<=12){
+      price= 8
+  }
+  if(age>=13 &&  age<=17){
+    price= 12
+  }
+  if(age>=18 && age<=59){
+    price= 15
+  }
+  if(age >=60){
+    price= 10
+  }
+  if(isWeekend){
+    price=price+3
+  }
+  return price
 }
